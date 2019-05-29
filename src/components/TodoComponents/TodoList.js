285ebf03d1436e3d7bbc6cd30d565a60197049f9
alexisjcarr <acarr59@gmail.com>
2019-05-29 18:03:05 -0500

    Day 1 Completed

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = props => {
+  return (
+    <div>
+      <h1>Todo List: MVP</h1>
+      {props.todoArray.map(el => (
+        <Todo todoItem={el.task} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
